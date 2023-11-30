2f0bd15a423c648328750f43b33f84735cb71d02
enzo-mir <miraglioenzo93@gmail.com>
2023-11-30 22:39:56 +0100

    feat(css/js) : responsive app, nav burger btn

---
+++ main.js
@@ -0,0 +1,29 @@
+const nav = document.querySelector("nav");
+const burgerBtn = document.getElementById("burgerMenu");
+
+burgerBtn.addEventListener("click", (e) => {
+  const elem = document.createElement("div");
+  elem.classList.add("overlay");
+  e.stopPropagation();
+  nav.classList.toggle("display");
+  e.target.classList.toggle("open");
+
+  nav.addEventListener("click", (evt) => evt.stopPropagation());
+  window.addEventListener("scroll", () => {
+    nav.classList.remove("display");
+    e.target.classList.remove("open");
+    document.body.removeChild(document.querySelector(".overlay"));
+  });
+
+  document.body.addEventListener("click", () => {
+    nav.classList.remove("display");
+    e.target.classList.remove("open");
+    document.body.removeChild(document.querySelector(".overlay"));
+  });
+
+  if (nav.classList.contains("display")) {
+    document.body.prepend(elem);
+  } else {
+    document.body.removeChild(document.querySelector(".overlay"));
+  }
+});
